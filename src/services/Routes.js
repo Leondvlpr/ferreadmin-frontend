3799918e5630e08aea4cefce3f5fdaadf304806c
Leondvlpr <leondvlpr@gmail.com>
2024-06-04 10:46:08 -0500

    Nuevas rutas servicios

--- src/services/Routes.js
+++ src/services/Routes.js
@@ -1,9 +1,9 @@
 export const ApiHost = "http://localhost:8092/api"
 
 export const userAuthentication = ApiHost + "/auth/login"
-export const obtenerInsumos = ApiHost + "/insumos";
-export const guardarInsumo = ApiHost + "/insumos/guardarInsumo"
-export const obtenerProveedores = ApiHost + "/proveedor"
-export const obtenerCompras = ApiHost + "/compras"
-export const guardarCompra = ApiHost + "/guardarCompra"
+export const obtenerInsumos = ApiHost + "/resources";
+export const guardarInsumo = ApiHost + "/resources/saveResource"
+export const obtenerProveedores = ApiHost + "/supplier/getSupplier"
+export const obtenerCompras = ApiHost + "/purchases"
+export const guardarCompra = ApiHost + "/purchases/savePurchase"
 export const generarPdf = ApiHost + "/generarPdf"
